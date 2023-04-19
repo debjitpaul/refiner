015c35d58b1530a50027e3b1aa81a854439ce7db
Debjit Paul <deb_4u22@yahoo.com>
2023-04-19 15:27:00 +0200

    Update run_critique.sh

--- run_critique.sh
+++ run_critique.sh
@@ -1,3 +1,3 @@
-export PYTHONPATH="${PYTHONPATH}:/home/paul/gpt-j-generation"
+export PYTHONPATH="${PYTHONPATH}:/home/"
 echo "equation_generation"
 python3 t5_experiments/scripts/train_predict.py --training-file data/mwp/critique_train.json --validation-file data/mwp/critique_val.json --language-model google/flan-t5-base --model-dir flan_t5_large_model --critique_model-dir output_critique  --epochs 10 --batch-size 2 --number_turn 4
