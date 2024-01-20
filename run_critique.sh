c7c9a1e05069a19b7d576a210824f36414081836
Debjit Paul <deb_4u22@yahoo.com>
2024-01-20 19:22:52 +0100

    Update run_critique.sh
    updated

--- run_critique.sh
+++ run_critique.sh
@@ -1,3 +1,1 @@
-export PYTHONPATH="${PYTHONPATH}:/home/"
-echo "equation_generation"
-python3 src/scripts/train_refiner.py --training-file data/mwp/critique_train.json --validation-file data/mwp/critique_val.json --language-model google/flan-t5-base --model-dir flan_t5_large_model --critique_model-dir output_critique --lora True --epochs 10 --batch-size 2 --number_turn 4
+python3 src/scripts/train_refiner.py --training-file data/critique_train.json --validation-file data/critique_val.json --language-model google/flan-t5-base --model-dir path_refiner_model --critique_model-dir path_critic_model --lora True --epochs 10 --batch-size 8 --number_turn 4
